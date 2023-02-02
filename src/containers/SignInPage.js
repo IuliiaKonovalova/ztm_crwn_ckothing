283ac6b2e10230e4279183a4795e78f9cdfe80b9
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-02 16:41:07 +0700

    Add link to sign up page

--- src/containers/SignInPage.js
+++ src/containers/SignInPage.js
@@ -1,7 +1,23 @@
-const SignInPage = () => (
-  <div>
-    <h1>Sign In</h1>
-  </div>
-);
+import Layout from "../components/Layout";
+import { signInWithGooglePopup } from "../utils/firebase/firebase.utils";
+
+const SignInPage = () => {
+
+  const logGoogleUser = async() => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  }
+  return (
+    <Layout title="Sign In">
+      <h1>Sign In Page</h1>
+      <button
+        className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+        onClick={logGoogleUser}
+      >
+        Sign In with Google
+      </button>
+    </Layout>
+  )
+}
 
 export default SignInPage;
