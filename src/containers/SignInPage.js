38aa943cd589e820c2cae49a426b656eb036d8d2
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-02 19:59:17 +0700

    Change signup google method to redirect

--- src/containers/SignInPage.js
+++ src/containers/SignInPage.js
@@ -1,24 +1,40 @@
 import Layout from "../components/Layout";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  auth,
 } from "../utils/firebase/firebase.utils";
 
 const SignInPage = () => {
+  useEffect(() => {
+    const logGoogleRedirectUser = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+        console.log(userDocRef);
+      }
+    };
+    logGoogleRedirectUser();
+  }, []);
 
-  const logGoogleUser = async() => {
-    const {user} = await signInWithGooglePopup();
+  const logGoogleRedirectUser = async() => {
+    const {user} = await signInWithGoogleRedirect();
     const userDocRef = await createUserDocumentFromAuth(user)
     console.log(userDocRef);
   }
+
   return (
     <Layout title="Sign In">
       <h1>Sign In Page</h1>
       <button
         className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
-        onClick={logGoogleUser}
+        onClick={logGoogleRedirectUser}
       >
-        Sign In with Google
+        Sign In with Google Redirect
       </button>
     </Layout>
   )
