262b12095358314993e7df8c4cfc29876b6a6e59
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-03 16:29:34 +0700

    Add separate component for google auth

---
+++ src/components/GoogleAuthentication.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { getRedirectResult } from "firebase/auth";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+  signInWithGoogleRedirect,
+  auth,
+} from "../utils/firebase/firebase.utils";
+
+
+const GoogleAuthenticationBlock = () => {
+
+  useEffect(() => {
+    const logGoogleRedirectUser = async () => {
+      const response = await getRedirectResult(auth);
+      console.log(response);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+        console.log(userDocRef);
+      }
+    };
+    logGoogleRedirectUser();
+  }, []);
+
+
+  const logGoogleRedirectUser = async() => {
+    const {user} = await signInWithGoogleRedirect();
+    const userDocRef = await createUserDocumentFromAuth(user)
+    console.log(userDocRef);
+  }
+
+  return (
+    <button
+      className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+      onClick={logGoogleRedirectUser}
+    >
+      Continue with Google
+    </button>
+  )
+}
+
+export default GoogleAuthenticationBlock;
