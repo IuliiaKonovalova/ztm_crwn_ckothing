725dfd71202912a742ddbdda531ef67a7544ec6b
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-03 15:24:11 +0700

    Add get data for firebase functionality

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,27 +1,67 @@
 import { useState } from "react";
-
-const defaultFormFields = {
-  name: "",
-  email: "",
-  password: "",
-  confirmPassword: "",
-};
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../utils/firebase/firebase.utils";
 
 const SignUpForm = () => {
 
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { name, email, password, confirmPassword } = formFields;
+  const handleSubmit = async (data, form) => {
+    console.log(data);
+    console.log(formData);
+    if (data.password !== data.confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    } else if (data.email === "" || data.password === "") {
+      alert("Email and password are required");
+      return;
+    } else if (data.password.length < 6) {
+      alert("Password must be at least 6 characters long");
+      return;
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
+      alert("Please enter a valid email address");
+      return;
+    }
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        data.email,
+        data.password
+      );
+      await createUserDocumentFromAuth(user, { name: data.name });
+    } catch (error) {
+      console.log("User creation error: ", error);
+      if(error.code === "auth/email-already-in-use") {
+        alert("Email already in use");
+      } else {
+        console.log("User creation error: ", error);
+      }
+    }
+  };
 
-  const handleFormChange = (e) => {
-    const { name, value } = e.target;
-    setFormFields({ ...formFields, [name]: value });
+  const onFieldChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return(
     <div>
       <h2>Sign Up with your email and password</h2>
-      <form>
+      <form
+        className="w-full max-w-sm"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit(formData);
+        }}
+        >
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
           type="text"
@@ -35,8 +75,8 @@ const SignUpForm = () => {
           type="text"
           placeholder="Name"
           required
-          onChange={handleFormChange}
-          value={name}
+          onChange={onFieldChange}
+          name="name"
         />
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
@@ -50,8 +90,8 @@ const SignUpForm = () => {
           type="email"
           placeholder="Email"
           required
-          onChange={handleFormChange}
-          value={email}
+          onChange={onFieldChange}
+          name="email"
         />
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
@@ -65,8 +105,8 @@ const SignUpForm = () => {
           type="password"
           placeholder="Password"
           required
-          onChange={handleFormChange}
-          value={password}
+          onChange={onFieldChange}
+          name="password"
         />
         <label
           className="block text-gray-700 text-sm font-bold mb-2"
@@ -80,9 +120,15 @@ const SignUpForm = () => {
           type="password"
           placeholder="Confirm Password"
           required
-          onChange={handleFormChange}
-          value={confirmPassword}
+          onChange={onFieldChange}
+          name="confirmPassword"
         />
+        <button
+          className="bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
+          type="submit"
+        >
+          Sign Up
+        </button>
       </form>
     </div>
   )
