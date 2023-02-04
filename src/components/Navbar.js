4d03113950fcee992fb544fcbd94423e962373c8
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-04 14:57:18 +0700

    Add current user control in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,22 @@
+import { useContext, useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { UserContext } from "../context/user.context"
 
 const Navbar = () => {
+  const [auth, setAuth] = useState(false)
+  // Set the current user
+  const { currentUser } = useContext(UserContext)
+  console.log(currentUser)
+  // console.log(currentUser.email)
+  useEffect(() => {
+    if (currentUser !== null && currentUser !== undefined) {
+      setAuth(true)
+      console.log("auth is true")
+    } else {
+      setAuth(false)
+      console.log("auth is false")
+    }
+  }, [currentUser])
   return(
     <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-6">
         <ul className="flex uppercase w-full justify-between text-white font-bold">
@@ -10,12 +26,28 @@ const Navbar = () => {
           <li className="mr-6">
             <Link className=" hover:text-pink-300" to="/shop">Shop</Link>
           </li>
-          <li className="mr-6">
-            <Link className="hover:text-pink-300" to="/signin">Sign In</Link>
-          </li>
-          <li className="mr-6">
-            <Link className="hover:text-pink-300" to="/signup">Sign Up</Link>
-          </li>
+          {auth ? (
+            <>
+              <li className="mr-6">
+                <Link
+                  className="hover:text-pink-300"
+                  to="/signin"
+                >
+                  Sign Out
+                  </Link>
+              </li>
+            </>
+          ) : (
+            <>
+            <li className="mr-6">
+              <Link className="hover:text-pink-300" to="/signin">Sign In</Link>
+            </li>
+            <li className="mr-6">
+              <Link className="hover:text-pink-300" to="/signup">Sign Up</Link>
+            </li>
+          </>
+          )
+          }
         </ul>
     </nav>
   )
