d14e421ca0cf0da00ea33ee0d79541c3c226e5f5
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-04 14:54:32 +0700

    Add user component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
+import { UserProvider } from './context/user.context';
 
 const HomePage = lazy(() => import("./containers/HomePage"));
 const ShopPage = lazy(() => import("./containers/ShopPage"));
@@ -11,14 +12,16 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/shop" element={<ShopPage />} />
-            <Route path="/signin" element={<SignInPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-          </Routes>
-        </Suspense>
+        <UserProvider>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/shop" element={<ShopPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+          </Suspense>
+        </UserProvider>
       </BrowserRouter>
     </>
   );
