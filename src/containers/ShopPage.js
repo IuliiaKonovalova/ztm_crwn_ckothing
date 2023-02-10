a5503a7175fa23360c96cc6c0ed5311a82197fdb
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-10 15:03:17 +0700

    Add button add to bag to products

--- src/containers/ShopPage.js
+++ src/containers/ShopPage.js
@@ -1,10 +1,13 @@
 import Layout from "../components/Layout";
 import { useContext } from "react";
 import { ProductsContext } from "../context/products.context";
+import { BagContext } from "../context/bag.context"
 
 const ShopPage = () => {
 
   const { products } = useContext(ProductsContext);
+
+  const { addItemsToBag } = useContext(BagContext);
 
   return (
     <Layout title="Products">
@@ -32,6 +35,41 @@ const ShopPage = () => {
                   </div>
                   <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                     <h2 className="text-lg font-semibold mb-4">{product.name}</h2>
+                  </div>
+                  <div className="flex items-center justify-between px-4 py-3">
+                    <div className="flex items-center">
+                      <div className="flex items-center">
+                        <button
+                          onClick={() => addItemsToBag(product)}
+                          className="p-1 text-gray-400 rounded-full hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                          aria-label="Add to bag"
+                        >
+                          <span
+                            className="hover:text-gray-500"
+                          >
+                            Add to bag
+                          </span>
+                          <svg
+                            className="w-5 h-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            aria-hidden="true"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M3 3a1 1 0 011-1h12a1 1 0 011 1v14a1 1 0 01-1 1H4a1 1 0 01-1-1V3zm3 0a1 1 0 00-1 1v12a1 1 0 001 1h8a1 1 0 001-1V4a1 1 0 00-1-1H6z"
+                              clipRule="evenodd"
+                            />
+                            <path
+                              fillRule="evenodd"
+                              d="M8 6a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
                   </div>
               </a>
             </div>
