74fa6483c33c3b19df1c84a0383aaaefa12e475f
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-10 17:57:35 +0700

    Change stylings for bag quantity

--- src/components/BagDropdown.js
+++ src/components/BagDropdown.js
@@ -29,7 +29,7 @@ const BagDropdown = ( bagProps ) => {
                   <p className="text-sm text-gray-500">{item.price}</p>
                 </div>
                 <div className="flex items-center">
-                  <p className="font-medium text-gray-900 ml-4">
+                  <p className="font-medium text-gray-900 ml-4 text-xs lowercase">
                     x <span className="font-medium">{item.quantity}</span>
                   </p>
                 </div>
