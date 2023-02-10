be140d986ffa0373fcc65ad3e2de5901355f9667
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-10 14:06:03 +0700

    Add products control to bag context

--- src/context/bag.context.js
+++ src/context/bag.context.js
@@ -1,24 +1,43 @@
 import { createContext, useState } from "react";
 
 
+const addBagItems = (bagProducts, productToAddToBag) => {
 
+  const existingBagItem = bagProducts.find(
+    (bagProduct) => bagProduct.id === productToAddToBag.id
+  );
+
+  if (existingBagItem) {
+    return bagProducts.map((bagProduct) =>
+      bagProduct.id === productToAddToBag.id
+        ? { ...bagProduct, quantity: bagProduct.quantity + 1 }
+        : bagProduct
+    );
+  }
+  return [...bagProducts, { ...productToAddToBag, quantity: 1 }]
+}
 
 export const BagContext = createContext({
-  products: [],
   isBagDropdownOpen: false,
   setIsBagDropdownOpen: () => {},
+  bagProducts: [],
+  addProductToBag: () => {},
 });
 
 export const BagProvider = ({ children }) => {
-  // const [products, setProducts] = useState(PRODUCTS_DATA);
+  const [bagProducts, setProductToBag] = useState([]);
+
+  const addItemsToBag = (productToAddToBag) => {
+    setProductToBag(addBagItems(bagProducts, productToAddToBag))
+  }
 
   const [isBagDropdownOpen, setIsBagDropdownOpen] = useState(false);
 
   const value = {
     isBagDropdownOpen,
     setIsBagDropdownOpen,
-    // products,
-    // setProducts,
+    bagProducts,
+    addItemsToBag,
   };
 
   return (
