8b7635c583251c2e75a789b5fd327f34aff9d97b
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-11 18:42:08 +0700

    Add show bag items in the BagPage functionality

--- src/containers/BagPage.js
+++ src/containers/BagPage.js
@@ -21,7 +21,32 @@ const BagPage = () => {
         <h1 className="text-4xl font-semibold my-16 text-center">
           Bag
         </h1>
-        
+        {bagProducts.length > 0 ? (
+          <div className="flex flex-col items-center">
+            {bagProducts.map((product) => {
+              return (
+                <div className="flex flex-col items-center">
+                <h1>{product.name}</h1>
+                <h1>{product.price}</h1>
+                <h1>{product.quantity}</h1>
+              </div>
+              )
+            }
+            )}
+          </div>
+          ) : (
+            <div className="flex flex-col items-center">
+              <h1 className="text-2xl font-semibold my-6 text-center">
+                Your bag is empty
+              </h1>
+              <Link to="/shop">
+                <button className="bg-black text-white px-4 py-2 rounded-lg">
+                  Shop
+                </button>
+              </Link>
+            </div>
+          )
+        }
 
       </div>
 
