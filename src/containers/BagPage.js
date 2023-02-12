7097c6189ec201b1c7e6ef9855b4972253676f08
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-12 14:56:48 +0700

    Fix talbe paddings and images

--- src/containers/BagPage.js
+++ src/containers/BagPage.js
@@ -71,61 +71,61 @@ const BagPage = () => {
                   {bagProducts.map((product) => {
                     return (
                       <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                      <td class="flex items-center py-2">
-                          <img
-                            src={product.imageUrl}
-                            alt="Apple Imac"
-                            />
-                      </td>
-                      <td class="pl-2 py-2 font-semibold text-gray-900 dark:text-white">
-                          {product.name}
-                      </td>
-                      <td class=" py-2">
-                          <div class="flex items-center space-x-3">
-                              <button class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                  <span class="sr-only">Quantity button</span>
-                                  <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-                              </button>
-                              <div>
-                                  <input type="number" id="second_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="1"
-                                    required
-                                  />
-                              </div>
-                              <button class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
-                                  <span class="sr-only">Quantity button</span>
-                                  <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
-                              </button>
-                          </div>
-                      </td>
-                      <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                          ${product.price}
-                      </td>
-                      <td class="px-6 py-4">
-                        <button
-                          className="p-1 text-gray-400 rounded-full
-                          hover:bg-gray-100 focus:outline-none focus:ring-2
-                          focus:ring-inset focus:ring-indigo-500 cursor-pointer"
-                          aria-label="Remove"
-                          onClick={() => {}}
-                        >
-                          <span className="sr-only">Remove</span>
-                          <svg
-                            className="w-5 h-5"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            aria-hidden="true"
+                        <td class="flex items-center py-2 sm:w-20">
+                            <img
+                              src={product.imageUrl}
+                              alt="Apple Imac"
+                              />
+                        </td>
+                        <td class="pl-2 py-2 font-semibold text-gray-900 dark:text-white">
+                            {product.name}
+                        </td>
+                        <td class=" py-2">
+                            <div class="flex items-center space-x-3">
+                                <button class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                                    <span class="sr-only">Quantity button</span>
+                                    <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                                </button>
+                                <div>
+                                    <input type="number" id="second_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                      placeholder="1"
+                                      required
+                                    />
+                                </div>
+                                <button class="inline-flex items-center p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                                    <span class="sr-only">Quantity button</span>
+                                    <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                                </button>
+                            </div>
+                        </td>
+                        <td class="py-2 font-semibold text-gray-900 dark:text-white text-center">
+                            ${product.price}
+                        </td>
+                        <td class="py-2 text-center">
+                          <button
+                            className="p-1 text-gray-400 rounded-full
+                            hover:bg-gray-100 focus:outline-none focus:ring-2
+                            focus:ring-inset focus:ring-indigo-500 cursor-pointer"
+                            aria-label="Remove"
+                            onClick={() => {}}
                           >
-                            <path
-                              fillRule="evenodd"
-                              d="M3 5a1 1 0 011-1h12a1 1 0 011 1v1H3V5zm12 2v9a2 2 0 01-2 2H7a2 2 0 01-2-2V7h10zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </button>
-                      </td>
-                  </tr>
+                            <span className="sr-only">Remove</span>
+                            <svg
+                              className="w-5 h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              aria-hidden="true"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 011 1v1H3V5zm12 2v9a2 2 0 01-2 2H7a2 2 0 01-2-2V7h10zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1z"
+                                clipRule="evenodd"
+                              />
+                            </svg>
+                          </button>
+                        </td>
+                      </tr>
                     )
                   })}
                 </tbody>
