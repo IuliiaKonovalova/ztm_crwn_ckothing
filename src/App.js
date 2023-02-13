b0a12e735ed36df56decc2c1999af4d8ecdcd0e5
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-13 13:19:07 +0700

    Add route for checkout

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const ShopPage = lazy(() => import("./containers/ShopPage"));
 const SignInPage = lazy(() => import("./containers/SignInPage"));
 const SignUpPage = lazy(() => import("./containers/SignUpPage"));
 const BagPage = lazy(() => import("./containers/BagPage"));
+const CheckoutPage = lazy(() => import("./containers/CheckoutPage"));
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
                   <Route path="/signin" element={<SignInPage />} />
                   <Route path="/signup" element={<SignUpPage />} />
                   <Route path="/bag" element={<BagPage />} />
+                  <Route path="/checkout" element={<CheckoutPage />} />
                 </Routes>
               </Suspense>
             </BagProvider>
