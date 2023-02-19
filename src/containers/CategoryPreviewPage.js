d2bc534e697d514d0fe0393601e9504133ccd397
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-19 19:26:00 +0700

    Add filtered products by category page logic

--- src/containers/CategoryPreviewPage.js
+++ src/containers/CategoryPreviewPage.js
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 import { useContext, useEffect, useState } from "react"
 import { CategoriesContext } from "../context/categories.context";
 
-const CategoryPreviewPage = () => {
+const CategoryPreviewPage = ({title, products}) => {
 
 
   return (
@@ -10,8 +10,17 @@ const CategoryPreviewPage = () => {
 
       <div className=" flex-auto flex-col items-center my-4 max-w-screen-xl">
         <h1 className="text-4xl font-semibold my-16 text-center">
-          Category Preview
+          Category: {title.toUpperCase()}
         </h1>
+        <div className="grid items-center">
+          {products.filter((product, index) => index < 4).map((product) => (
+            <div key={product.id}>
+              <div className="flex flex-col items-center justify-center">
+                {product.name}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </Layout>
   );
