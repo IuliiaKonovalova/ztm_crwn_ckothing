4b1fa259054ef2cd6c488c89dc6c4ec0d20feb0c
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-02-20 18:52:32 +0700

    Add stylings and code structure

--- src/containers/CategoryPreviewPage.js
+++ src/containers/CategoryPreviewPage.js
@@ -19,34 +19,27 @@ const CategoryPreviewPage = ({title, products}) => {
   return (
     <Layout title="Products">
 
-      <div className="flex-auto flex-col items-center my-4  w-full bg-green-700">
-        <h1 className="text-4xl font-semibold my-16 text-center">
-          Category: {title}
-        </h1>
-        <div className="flex flex-col justify-center items-center  w-full bg-pink-900">
+      <div className="flex-auto flex-col items-center my-4  w-full">
+        <div className="flex flex-col justify-center w-full">
           <>
           { Object.keys(categoriesMap).map((category) => {
             return (
-<>
-
-            <h2 className="text-2xl font-semibold my-6 text-start">
-              {category}
-            </h2>
-            <div className="flex w-full flex-wrap justify-center items-center bg-purple-500">
-              {categoriesMap[category].filter((product, index) =>
-                                index < 4).map((product) =>
-                {
-                return(
-                  <div class="w-full sm:w-1/2 md:w-1/3 xl:w-1/5 p-4">
-                    <ProductCard product={product} addItemFromShopToBag={addItemFromShopToBag}/>
-                  </div>
-                )
-              })}
-            </div>
-</>
+              <>
+                <h2 className="text-2xl font-semibold my-2 ml-2 text-left">
+                  {category.toUpperCase()} 
+                </h2>
+                <div className="flex flex-wrap justify-center items-center">
+                  {categoriesMap[category].filter((product, index) =>
+                                    index < 3).map((product) =>
+                    {
+                    return(
+                      <ProductCard product={product} addItemFromShopToBag={addItemFromShopToBag}/>
+                    )
+                  })}
+                </div>
+              </>
             )
           })}
-
           </>
         </div>
       </div>
