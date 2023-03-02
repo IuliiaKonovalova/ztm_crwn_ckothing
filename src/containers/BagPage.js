977b43f487f61fbfa5605ac6f1f154df9786464f
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-03-02 13:09:15 +0700

    Fix clipRule name

--- src/containers/BagPage.js
+++ src/containers/BagPage.js
@@ -134,7 +134,7 @@ const BagPage = () => {
                                   <path
                                     fill-rule="evenodd"
                                     d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clip-rule="evenodd"
+                                    clipRule="evenodd"
                                   >
                                   </path>
                               </svg>
@@ -168,7 +168,7 @@ const BagPage = () => {
                                 <path
                                   fill-rule="evenodd"
                                   d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                  clip-rule="evenodd"
+                                  clipRule="evenodd"
                                 >
                                 </path>
                               </svg>
