2b06b76be79e60b42705fbcefca48e892059c93d
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-03-03 14:32:53 +0700

    Add conditional for empty bag

--- src/components/BagDropdown.js
+++ src/components/BagDropdown.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { BagContext } from "../context/bag.context";
 
 const BagDropdown = ( bagProps ) => {
-  const { bagShow } = bagProps;
+  const { bagShow } = bagProps; 
   const { removeItemsFromBag } = useContext(BagContext);
 
   const removeItemFromBagFromDropdown = (item) => {
@@ -19,57 +19,63 @@ const BagDropdown = ( bagProps ) => {
         >
         <div className="py-1">
           
-          {bagShow.map((item) => {
-            return (
-            <div
-              className=" grid grid-cols-6 gap-3 py-4 px-3"
-              key={item.id}
-            >
-              <div className="flex items-center col-span-4">
-                <img
-                  className="w-8 h-8 mr-2 rounded-full"
-                  src={item.imageUrl}
-                  alt=" "
-                />
-                <div>
-                  <p className="text-sm font-medium text-gray-900">
-                    {item.name}
+          {bagShow.length ? (
+            bagShow.map((item) => {
+              return (
+              <div
+                className=" grid grid-cols-6 gap-3 py-4 px-3"
+                key={item.id}
+              >
+                <div className="flex items-center col-span-4">
+                  <img
+                    className="w-8 h-8 mr-2 rounded-full"
+                    src={item.imageUrl}
+                    alt=" "
+                  />
+                  <div>
+                    <p className="text-sm font-medium text-gray-900">
+                      {item.name}
+                    </p>
+                    <p className="text-sm text-gray-500">{item.price}</p>
+                  </div>
+                </div>
+                <div className="flex items-center col-span-1">
+                  <p className="font-medium text-gray-900 text-xs lowercase">
+                    x <span className="font-medium">{item.quantity}</span>
                   </p>
-                  <p className="text-sm text-gray-500">{item.price}</p>
+                </div>
+                <div className="flex items-center col-span-1">
+                  <button
+                    className="p-1 text-gray-400 rounded-full
+                    hover:bg-gray-100 focus:outline-none focus:ring-2
+                    focus:ring-inset focus:ring-indigo-500 cursor-pointer"
+                    aria-label="Remove"
+                    onClick={() => removeItemFromBagFromDropdown(item)}
+                  >
+                    <span className="sr-only">Remove</span>
+                    <svg
+                      className="w-5 h-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 011 1v1H3V5zm12 2v9a2 2 0 01-2 2H7a2 2 0 01-2-2V7h10zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </button>
                 </div>
               </div>
-              <div className="flex items-center col-span-1">
-                <p className="font-medium text-gray-900 text-xs lowercase">
-                  x <span className="font-medium">{item.quantity}</span>
-                </p>
-              </div>
-              <div className="flex items-center col-span-1">
-                <button
-                  className="p-1 text-gray-400 rounded-full
-                  hover:bg-gray-100 focus:outline-none focus:ring-2
-                  focus:ring-inset focus:ring-indigo-500 cursor-pointer"
-                  aria-label="Remove"
-                  onClick={() => removeItemFromBagFromDropdown(item)}
-                >
-                  <span className="sr-only">Remove</span>
-                  <svg
-                    className="w-5 h-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M3 5a1 1 0 011-1h12a1 1 0 011 1v1H3V5zm12 2v9a2 2 0 01-2 2H7a2 2 0 01-2-2V7h10zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1zm-4 0a1 1 0 00-1 1v5a1 1 0 002 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </button>
-              </div>
+              )
+            })
+          ) : (
+            <div className="flex justify-center py-4">
+              <p className="text-sm text-gray-500">Your bag is empty</p>
             </div>
-            )
-          })}
+          )}
         </div>
         <div className="px-4 py-3">
           <p className="text-sm text-gray-500">
