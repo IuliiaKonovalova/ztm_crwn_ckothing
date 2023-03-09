e509af5507252a6762b99479b0be3e67871e0b5f
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-03-09 13:46:27 +0700

    Update default status for userReducer

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -6,13 +6,13 @@ const INITIAL_STATE = {
   currentUser: null,
 };
 
-const userReducer = (state, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
       return { ...state, currentUser: payload };
     default:
-      throw new Error(`Unhandled type ${type} in userReducer`);
+      return state;
   }
 };
