612329a2789f7d0eda34e3f73f13ee1877588b74
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-03-10 12:08:48 +0700

    Add userReducer to root Reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -3,6 +3,6 @@ import { combineReducers } from "redux";
 import { userReducer } from "./user/user.reducer";
 
 export const rootReducer = combineReducers({
-  // Add reducers here
+  user: userReducer,
 
 });
