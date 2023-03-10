3c84a6716f66b134d8f44a87dd31d11ecc55dd0a
IuliiaKonovalova <juliiakonn@protonmail.com>
2023-03-10 12:09:18 +0700

    Add Provide with store object

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { lazy, Suspense } from 'react';
+import { Provider } from 'react-redux';
 import { UserProvider } from './context/user.context';
 import { CategoriesProvider } from './context/categories.context';
 import { BagProvider } from './context/bag.context';
+import { store } from './store/store';
 
 const HomePage = lazy(() => import("./containers/HomePage"));
 const ShopPage = lazy(() => import("./containers/ShopPage"));
@@ -17,23 +19,25 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <UserProvider>
-          <CategoriesProvider>
-            <BagProvider>
-              <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                  <Route path="/" element={<CategoryPreviewPage />} />
-                  <Route path="/shop/*" element={<ShopPage />} />
-                  {/* <Route path="/category" element={<CategoryPreviewPage />} /> */}
-                  <Route path="/signin" element={<SignInPage />} />
-                  <Route path="/signup" element={<SignUpPage />} />
-                  <Route path="/bag" element={<BagPage />} />
-                  <Route path="/checkout" element={<CheckoutPage />} />
-                </Routes>
-              </Suspense>
-            </BagProvider>
-          </CategoriesProvider>
-        </UserProvider>
+        <Provider store={store}>
+          <UserProvider>
+            <CategoriesProvider>
+              <BagProvider>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Routes>
+                    <Route path="/" element={<CategoryPreviewPage />} />
+                    <Route path="/shop/*" element={<ShopPage />} />
+                    {/* <Route path="/category" element={<CategoryPreviewPage />} /> */}
+                    <Route path="/signin" element={<SignInPage />} />
+                    <Route path="/signup" element={<SignUpPage />} />
+                    <Route path="/bag" element={<BagPage />} />
+                    <Route path="/checkout" element={<CheckoutPage />} />
+                  </Routes>
+                </Suspense>
+              </BagProvider>
+            </CategoriesProvider>
+          </UserProvider>
+        </Provider>
       </BrowserRouter>
     </>
   );
